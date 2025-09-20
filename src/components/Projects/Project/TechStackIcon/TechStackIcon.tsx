86f2cd93c02cf1e0225437b08fbb738df30e2561
Tyler Tierney <tytierney@yahoo.com>
2025-09-20 17:45:13 -0400

    update projects list

--- src/components/Projects/Project/TechStackIcon/TechStackIcon.tsx
+++ src/components/Projects/Project/TechStackIcon/TechStackIcon.tsx
@@ -1,4 +1,4 @@
-import "./TechStackIcon.css";
+import './TechStackIcon.css'
 import {
   amazonS3Icon,
   angularIcon,
@@ -6,6 +6,7 @@ import {
   chakraIcon,
   emailIcon,
   firebaseIcon,
+  githubActionsIcon,
   herokuIcon,
   mongoDbIcon,
   netlifyIcon,
@@ -13,63 +14,68 @@ import {
   nodeIcon,
   reactIcon,
   socketIoIcon,
-} from "../../../../icons/icons";
-import { useState } from "react";
+  storybookIcon,
+} from '../../../../icons/icons'
+import { useState } from 'react'
 
 export type iconKey =
-  | "React"
-  | "Node JS"
-  | "Heroku"
-  | "Socket.IO"
-  | "Netlify"
-  | "Chakra UI"
-  | "Firebase"
-  | "Next.js"
-  | "MongoDB"
-  | "Auth0"
-  | "Angular"
-  | "Netlify Identity"
-  | "Amazon S3"
-  | "SendGrid"
-  | "Vite";
+  | 'React'
+  | 'Node JS'
+  | 'Heroku'
+  | 'Socket.IO'
+  | 'Netlify'
+  | 'Chakra UI'
+  | 'Firebase'
+  | 'Next.js'
+  | 'MongoDB'
+  | 'Auth0'
+  | 'Angular'
+  | 'Netlify Identity'
+  | 'Amazon S3'
+  | 'SendGrid'
+  | 'Vite'
+  | 'Github Actions'
+  | 'Storybook'
 
 const techStackIcons = new Map<iconKey, { icon: JSX.Element; color: string }>([
-  ["React", { icon: reactIcon, color: "#61dafb" }],
-  ["Node JS", { icon: nodeIcon, color: "#73aa63" }],
-  ["Heroku", { icon: herokuIcon, color: "#79589f" }],
-  ["Socket.IO", { icon: socketIoIcon, color: "silver" }],
-  ["Netlify", { icon: netlifyIcon, color: "#74bece" }],
-  ["Chakra UI", { icon: chakraIcon, color: "#4dc8c3" }],
-  ["Firebase", { icon: firebaseIcon, color: "#ffcb2d" }],
-  ["Next.js", { icon: nextJsIcon, color: "purple" }],
-  ["MongoDB", { icon: mongoDbIcon, color: "#12aa52" }],
-  ["Auth0", { icon: auth0Icon, color: "#eb5423" }],
-  ["Angular", { icon: angularIcon, color: "#dd0031" }],
-  ["Netlify Identity", { icon: netlifyIcon, color: "#74bece" }],
-  ["Amazon S3", { icon: amazonS3Icon, color: "#e08b2d" }],
-  ["SendGrid", { icon: emailIcon, color: "#61dafb" }],
-]);
+  ['React', { icon: reactIcon, color: '#61dafb' }],
+  ['Node JS', { icon: nodeIcon, color: '#73aa63' }],
+  ['Heroku', { icon: herokuIcon, color: '#79589f' }],
+  ['Socket.IO', { icon: socketIoIcon, color: 'silver' }],
+  ['Netlify', { icon: netlifyIcon, color: '#74bece' }],
+  ['Chakra UI', { icon: chakraIcon, color: '#4dc8c3' }],
+  ['Firebase', { icon: firebaseIcon, color: '#ffcb2d' }],
+  ['Next.js', { icon: nextJsIcon, color: 'purple' }],
+  ['MongoDB', { icon: mongoDbIcon, color: '#12aa52' }],
+  ['Auth0', { icon: auth0Icon, color: '#eb5423' }],
+  ['Angular', { icon: angularIcon, color: '#dd0031' }],
+  ['Netlify Identity', { icon: netlifyIcon, color: '#74bece' }],
+  ['Amazon S3', { icon: amazonS3Icon, color: '#e08b2d' }],
+  ['SendGrid', { icon: emailIcon, color: '#61dafb' }],
+  ['Github Actions', { icon: githubActionsIcon, color: 'green' }],
+  ['Storybook', { icon: storybookIcon, color: 'pink' }],
+])
 
 interface TechStackIconProps {
-  name: iconKey;
+  name: iconKey
 }
 
 export default function TechStackIcon({ name }: TechStackIconProps) {
-  const icon = techStackIcons.get(name)?.icon ?? reactIcon;
+  const icon = techStackIcons.get(name)?.icon ?? reactIcon
 
-  const [color, setColor] = useState("var(--text)");
+  const [color, setColor] = useState('var(--text)')
 
-  const hoverColor = techStackIcons.get(name)?.color ?? "var(--text)";
+  const hoverColor = techStackIcons.get(name)?.color ?? 'var(--text)'
 
   return (
     <div
       onMouseEnter={() => setColor(hoverColor)}
-      onMouseLeave={() => setColor("var(--text)")}
+      onMouseLeave={() => setColor('var(--text)')}
       style={{ color }}
       className="techStackIcon"
     >
       <div className="icon">{icon}</div>
       <span className="name">{name}</span>
     </div>
-  );
+  )
 }
--- src/projects.ts
+++ src/projects.ts
@@ -1,319 +1,241 @@
-import { iconKey } from "./components/Projects/Project/TechStackIcon/TechStackIcon";
+import { iconKey } from './components/Projects/Project/TechStackIcon/TechStackIcon'
+
+const githubUrl = 'https://github.com/tylertierney'
 
 export interface IProject {
-  title: string;
-  description: string;
-  hostedSite: string;
-  github: string;
-  featuredImage: string;
-  techStack: Array<{ name: iconKey; icon: string }>;
-  color: string;
+  title: string
+  description: string
+  hostedSite: string
+  github: string
+  featuredImage: string
+  techStack: Array<{
+    name: iconKey
+  }>
+  color: string
 }
 
 const projects: IProject[] = [
   {
-    title: "Kadoodle",
-    description:
-      "Kadoodle is a multiplayer drawing game. Players create a lobby, invite friends, and take turns drawing and guessing to score points. Kadoodle also supports group video chat.",
-    hostedSite: "https://kadoodle.us",
-    github: "https://github.com/tylertierney/kadoodle",
-    featuredImage: "/images/kadoodle_iphone_mockups.png",
+    title: 'Strings',
+    description: `Strings is a clone of the popular New York Times game Strands. It's not your typical word search.`,
+    hostedSite: 'https://strands-1.netlify.app',
+    github: `${githubUrl}/strands`,
+    featuredImage: '/images/strands_iphone_mockups.png',
     techStack: [
       {
-        name: "React",
-        icon: "FaReact",
+        name: 'React',
       },
       {
-        name: "Node JS",
-        icon: "FaNodeJs",
+        name: 'Netlify',
       },
       {
-        name: "Heroku",
-        icon: "SiHeroku",
-      },
-      {
-        name: "Socket.IO",
-        icon: "SiSocketDotIo",
-      },
-      {
-        name: "Netlify",
-        icon: "SiNetlify",
+        name: 'Github Actions',
       },
     ],
-    color: "#ffaa7c",
+    color: '',
   },
   {
-    title: "Chatmosphere",
-    description:
-      "Chatmosphere is an instant messaging platform. Users can sign-in securely via email/password, Github authentication, or anonymously.",
-    hostedSite: "https://chatmosphere.us",
-    github: "https://github.com/tylertierney/chatapp",
-    featuredImage: "/images/chatmosphere_iphone_mockups.png",
+    title: 'Movie Quiz',
+    description: `Given a blurred movie or TV show poster, try to choose the correct title.`,
+    hostedSite: 'https://movie-quiz-1.netlify.app',
+    github: `${githubUrl}/movie-quiz`,
+    featuredImage: '/images/movie-quiz_iphone_mockups.png',
     techStack: [
       {
-        name: "React",
-        icon: "FaReact",
+        name: 'React',
       },
       {
-        name: "Chakra UI",
-        icon: "BsLightningFill",
+        name: 'Node JS',
       },
       {
-        name: "Node JS",
-        icon: "FaNodeJs",
-      },
-      {
-        name: "Heroku",
-        icon: "SiHeroku",
-      },
-      {
-        name: "Firebase",
-        icon: "SiFirebase",
-      },
-      {
-        name: "Socket.IO",
-        icon: "SiSocketDotIo",
-      },
-      {
-        name: "Netlify",
-        icon: "SiNetlify",
+        name: 'Netlify',
       },
     ],
-    color: "#97e4f0",
+    color: '',
   },
   {
-    title: "Chord Finder",
-    description:
-      "Enter guitar notes on a virtual fretboard and identify the associated chord. Built using React, Typescript, and Nextjs.",
-    hostedSite: "https://www.chordfinder.us/",
-    github: "https://github.com/tylertierney/chordfinder",
-    featuredImage: "/images/chordfinder_iphone_mockups.png",
+    title: 'Connections',
+    description: `Another one of my New York Times clones, Connections is a game where you have four attempts to categorize related terms.`,
+    hostedSite: 'https://connections-1.netlify.app',
+    github: `${githubUrl}/strands`,
+    featuredImage: '/images/connections_iphone_mockups.png',
     techStack: [
       {
-        name: "React",
-        icon: "FaReact",
+        name: 'React',
       },
       {
-        name: "Chakra UI",
-        icon: "BsLightningFill",
+        name: 'Netlify',
       },
       {
-        name: "Next.js",
-        icon: "SiNextDotJs",
+        name: 'Github Actions',
       },
     ],
-    color: "#78DD78",
+    color: 'black',
   },
   {
-    title: "MovieWorm",
+    title: 'Kadoodle',
     description:
-      "MovieWorm is a platform where users can create private groups, invite friends, and share movie reviews and ratings.",
-    hostedSite: "https://movieworm.io",
-    github: "https://github.com/tylertierney/movieworm",
-    featuredImage: "/images/movieworm_iphone_mockups.png",
+      'Kadoodle is a multiplayer drawing game. Players create a lobby, invite friends, and take turns drawing and guessing to score points. Kadoodle also supports group video chat.',
+    hostedSite: 'https://kadoodle.us',
+    github: `${githubUrl}/kadoodle`,
+    featuredImage: '/images/kadoodle_iphone_mockups.png',
     techStack: [
       {
-        name: "React",
-        icon: "FaReact",
+        name: 'React',
       },
       {
-        name: "Chakra UI",
-        icon: "BsLightningFill",
+        name: 'Node JS',
       },
       {
-        name: "Node JS",
-        icon: "FaNodeJs",
+        name: 'Socket.IO',
       },
       {
-        name: "MongoDB",
-        icon: "SiMongodb",
+        name: 'Storybook',
       },
       {
-        name: "Auth0",
-        icon: "SiAuth0",
+        name: 'Netlify',
       },
     ],
-    color: "#ffa3a3",
+    color: '#ffaa7c',
   },
   {
-    title: "Custom Homepage",
-    description:
-      "This app, built entirely with Angular, provides a simple interface that allows users to generate and customize a browser homepage. Users can edit bookmarks, change the appearance and colors of the page, update their info, and select from a number of wallpapers.",
-    hostedSite: "https://tylertierney.github.io/angular-startpage/",
-    github: "https://github.com/tylertierney/angular-startpage",
-    featuredImage: "/images/startpage_iphone_mockups.png",
-    techStack: [{ name: "Angular", icon: "FaAngular" }],
-    color: "#A895DA",
-  },
-  {
-    title: "LinkBase",
-    description:
-      "LinkBase began as my introduction to Next.js. The project is a full-stack social media platform. User authentication is provided by Netlify Identity, database operations are managed through MongoDB, and image storage is provided by Amazon S3. The frontend uses Chakra UI in tandem with Next JS to provide server-side rendering, static page generation, and dynamic routing. All backend operations are written using Node JS.",
-    hostedSite: "https://link-base.netlify.app/",
-    github: "https://github.com/tylertierney/link-base",
-    featuredImage: "/images/iphone_mockups.png",
+    title: 'Minesweeper',
+    description: `My recreation of the classic game. Carefully uncover tiles while avoiding the bombs. Adjust the width and height of the grid, and the number of bombs.`,
+    hostedSite: 'https://tylertierney.github.io/minesweeper/',
+    github: `${githubUrl}/minesweeper`,
+    featuredImage: '/images/minesweeper_iphone_mockups.png',
     techStack: [
       {
-        name: "React",
-        icon: "FaReact",
-      },
-      {
-        name: "Chakra UI",
-        icon: "BsLightningFill",
-      },
-      {
-        name: "Node JS",
-        icon: "FaNodeJs",
-      },
-      {
-        name: "Netlify Identity",
-        icon: "SiNetlify",
-      },
-      {
-        name: "Next.js",
-        icon: "SiNextDotJs",
-      },
-      {
-        name: "MongoDB",
-        icon: "SiMongodb",
-      },
-      {
-        name: "Amazon S3",
-        icon: "FaAws",
+        name: 'React',
       },
     ],
-    color: "#1e90d6",
+    color: '',
   },
   {
-    title: "The Scoop",
+    title: 'Chatmosphere',
     description:
-      "The Scoop is my first attempt at building a web scraper. Instead of using an API, I parsed HTML from CNN Lite to build a search interface and email notification system. This was also my first project to fully integrate user authentication, in this case those services were provided by Firebase.",
-    hostedSite: "https://thescoop-news.herokuapp.com/",
-    github: "https://github.com/tylertierney/webscraper",
-    featuredImage: "",
+      'Chatmosphere is an instant messaging platform. Users can sign-in securely via email/password, Github authentication, or anonymously.',
+    hostedSite: 'https://chatmosphere.us',
+    github: `${githubUrl}/chatapp`,
+    featuredImage: '/images/chatmosphere_iphone_mockups.png',
     techStack: [
       {
-        name: "React",
-        icon: "FaReact",
+        name: 'React',
       },
       {
-        name: "Chakra UI",
-        icon: "BsLightningFill",
+        name: 'Chakra UI',
       },
       {
-        name: "Heroku",
-        icon: "SiHeroku",
+        name: 'Node JS',
       },
       {
-        name: "Firebase",
-        icon: "SiFirebase",
+        name: 'Heroku',
       },
       {
-        name: "SendGrid",
-        icon: "AiOutlineMail",
+        name: 'Firebase',
+      },
+      {
+        name: 'Socket.IO',
+      },
+      {
+        name: 'Netlify',
       },
     ],
-    color: "#FFBB5C",
+    color: '#97e4f0',
   },
-];
+  {
+    title: 'Chord Finder',
+    description:
+      'Enter guitar notes on a virtual fretboard and identify the associated chord. Built using React, Typescript, and Nextjs.',
+    hostedSite: 'https://www.chordfinder.us/',
+    github: `${githubUrl}/chordfinder`,
+    featuredImage: '/images/chordfinder_iphone_mockups.png',
+    techStack: [
+      {
+        name: 'React',
+      },
+      {
+        name: 'Chakra UI',
+      },
+      {
+        name: 'Next.js',
+      },
+    ],
+    color: '#78DD78',
+  },
+  {
+    title: 'MovieWorm',
+    description:
+      'MovieWorm is a platform where users can create private groups, invite friends, and share movie reviews and ratings.',
+    hostedSite: 'https://movieworm.io',
+    github: `${githubUrl}/movieworm`,
+    featuredImage: '/images/movieworm_iphone_mockups.png',
+    techStack: [
+      {
+        name: 'React',
+      },
+      {
+        name: 'Chakra UI',
+      },
+      {
+        name: 'Node JS',
+      },
+      {
+        name: 'MongoDB',
+      },
+      {
+        name: 'Auth0',
+      },
+    ],
+    color: '#ffa3a3',
+  },
+]
 
-export default projects;
+export default projects
 
-export type ISmallProject = Omit<IProject, "techStack">;
+export type ISmallProject = Omit<IProject, 'techStack'>
 
 export const smallerProjects: ISmallProject[] = [
   {
-    title: "Connections",
-    description: "Identify categories from the given words",
-    hostedSite: "https://connections-1.netlify.app/",
-    featuredImage: "/images/screenshots/connections_desktop.png",
-    color: "#7ce9d6",
-    github: "https://github.com/tylertierney/connections",
+    title: 'Angle',
+    description: 'Guess the angle in five tries!',
+    hostedSite: 'https://tylertierney.github.io/angle',
+    featuredImage: '/images/screenshots/angle_desktop.png',
+    color: '#ff9b9b',
+    github: `${githubUrl}/angle`,
   },
   {
-    title: "Movie Quiz",
-    description: "Guess the movie from its poster",
-    hostedSite: "https://movie-quiz-1.netlify.app/",
-    featuredImage: "/images/screenshots/movie-quiz_desktop.png",
-    color: "#7ce9d6",
-    github: "https://github.com/tylertierney/movie-quiz",
+    title: 'Snake',
+    description: 'A quick React game',
+    hostedSite: 'https://tylertierney.github.io/snake/',
+    featuredImage: '/images/screenshots/snake_desktop.png',
+    color: '#ff9f23',
+    github: `${githubUrl}/snake`,
   },
   {
-    title: "Minesweeper",
-    description: "My recreation of the classic game!",
-    hostedSite: "https://tylertierney.github.io/minesweeper",
-    featuredImage: "/images/screenshots/minesweeper_desktop.png",
-    color: "#A895DA",
-    github: "https://github.com/tylertierney/minesweeper",
-  },
-  {
-    title: "Angle",
-    description: "Guess the angle in five tries!",
-    hostedSite: "https://tylertierney.github.io/angle",
-    featuredImage: "/images/screenshots/angle_desktop.png",
-    color: "#ff9b9b",
-    github: "https://github.com/tylertierney/angle",
-  },
-  {
-    title: "Snake",
-    description: "A quick React game",
-    hostedSite: "https://tylertierney.github.io/snake/",
-    featuredImage: "/images/screenshots/snake_desktop.png",
-    color: "#ff9f23",
-    github: "https://github.com/tylertierney/snake",
-  },
-  {
-    title: "Wordle Clone",
+    title: 'Wordle Clone',
     description:
-      "A clone of the popular New York Times game, Wordle. Built with React and Typescript.",
-    hostedSite: "https://imaginative-griffin-b54662.netlify.app/",
-    featuredImage: "/images/screenshots/wordle_desktop.png",
-    color: "#7ce9d6",
-    github: "https://github.com/tylertierney/wordle",
+      'A clone of the popular New York Times game, Wordle. Built with React and Typescript.',
+    hostedSite: 'https://imaginative-griffin-b54662.netlify.app/',
+    featuredImage: '/images/screenshots/wordle_desktop.png',
+    color: '#7ce9d6',
+    github: `${githubUrl}/wordle`,
   },
   {
-    title: "Connect 4",
-    description: "My first Angular project!",
-    hostedSite: "https://tylertierney.github.io/connectfour/",
-    featuredImage: "/images/screenshots/connectfour_desktop.png",
-    color: "#88ce66",
-    github: "https://github.com/tylertierney/connectfour",
+    title: 'Connect 4',
+    description: 'My first Angular project!',
+    hostedSite: 'https://tylertierney.github.io/connectfour/',
+    featuredImage: '/images/screenshots/connectfour_desktop.png',
+    color: '#88ce66',
+    github: `${githubUrl}/connectfour`,
   },
   {
-    title: "Battleship",
+    title: 'Battleship',
     description:
-      "The classic board game, built with React and TypeScript. See if you can beat the computer!",
-    hostedSite: "https://tylertierney.github.io/battleship/",
-    featuredImage: "/images/screenshots/battleship_desktop.png",
-    color: "#ff9982",
-    github: "https://github.com/tylertierney/battleship",
+      'The classic board game, built with React and TypeScript. See if you can beat the computer!',
+    hostedSite: 'https://tylertierney.github.io/battleship/',
+    featuredImage: '/images/screenshots/battleship_desktop.png',
+    color: '#ff9982',
+    github: `${githubUrl}/battleship`,
   },
-
-  {
-    title: "Job Tracker",
-    description:
-      "This is a list component for tracking submitted job applications. If a user provides a link to the LinkedIn job posting, the site will scrape the page for the associated job description.",
-    hostedSite: "https://application-tracker123.herokuapp.com/",
-    featuredImage: "/images/screenshots/applicationtracker_desktop.png",
-    color: "#4cccd7",
-    github: "https://github.com/tylertierney/application-tracker",
-  },
-  {
-    title: "Startpage",
-    description:
-      "I used CSS Grid and React to create a simple homepage for all of my browsing activity. The page includes a dynamic greeting message, date and time, random quotes, Google search, and easily-clickable links to my most-used websites.",
-    hostedSite: "https://tylertierney.github.io/startpage/",
-    featuredImage: "/images/screenshots/startpage_desktop.png",
-    color: "#7ce9d6",
-    github: "https://github.com/tylertierney/startpage",
-  },
-  {
-    title: "Landing Page",
-    description:
-      "This is a fictional product landing page. Created with the purpose of testing out responsive design patterns and serving as a boilerplate Next.js project.",
-    hostedSite: "https://condescending-morse-95f608.netlify.app/",
-    featuredImage: "/images/screenshots/landingpage_desktop.png",
-    color: "#e43535",
-    github: "https://github.com/tylertierney/next-boilerplate",
-  },
-];
+]
